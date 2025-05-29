58b915e552f249e057166d490363b47d710eafe7
Ngocbao22 <107461196+Ngocbao22@users.noreply.github.com>
2025-05-29 15:10:33 +0700

    Service Detail

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,6 +5,10 @@ import Login from '../pages/public/Login';
 import Register from '../pages/public/Register';
 import Home from '../pages/public/Home';
 import UserService from '../pages/public/UserService';
+import BlogList from "../pages/public/BlogList";
+import BlogDetail from "../pages/public/BlogDetail";
+import DoctorList from "../pages/public/DoctorList";
+import DoctorProfile from "../pages/public/DoctorProfile";
 import RegisterService from '../pages/public/RegisterService';
 import ServiceDetail from "../pages/public/ServiceDetail";
 
@@ -14,6 +18,7 @@ import Dashboard from '../pages/admin/Dashboard';
 import Services from '../pages/admin/Services';
 import Feedback from '../pages/admin/Feedback';
 import Reports from '../pages/admin/Reports';
+import Manager from '../pages/admin/Manager';
 
 // Manager
 import ManagerLayout from '../layouts/ManagerLayout';
@@ -23,6 +28,7 @@ import Schedules from '../pages/manager/Schedules';
 import Registrations from '../pages/manager/Registrations';
 import Progress from '../pages/manager/Progress';
 import Notifications from '../pages/manager/Notifications';
+import BlogManagement from "../pages/manager/BlogManagement";
 
 const AppRouter = () => {
   return (
@@ -32,6 +38,10 @@ const AppRouter = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/userservice" element={<UserService />} />
+      <Route path="/blog" element={<BlogList />} />
+      <Route path="/blog/:id" element={<BlogDetail />} />
+      <Route path="/doctors" element={<DoctorList />} />
+      <Route path="/doctors/:id" element={<DoctorProfile />} />
       <Route path="/services/:serviceId" element={<ServiceDetail />} />
       <Route path="/register/:serviceId" element={<RegisterService />} />
 
@@ -40,6 +50,7 @@ const AppRouter = () => {
       <Route path="/admin/services" element={<AdminLayout><Services /></AdminLayout>} />
       <Route path="/admin/feedback" element={<AdminLayout><Feedback /></AdminLayout>} />
       <Route path="/admin/reports" element={<AdminLayout><Reports /></AdminLayout>} />
+      <Route path="/admin/manager" element={<AdminLayout><Manager /></AdminLayout>} />
 
       {/* Manager Routes */}
       <Route path="/manager/doctors" element={<ManagerLayout><Doctors /></ManagerLayout>} />
@@ -48,6 +59,7 @@ const AppRouter = () => {
       <Route path="/manager/registrations" element={<ManagerLayout><Registrations /></ManagerLayout>} />
       <Route path="/manager/progress" element={<ManagerLayout><Progress /></ManagerLayout>} />
       <Route path="/manager/notifications" element={<ManagerLayout><Notifications /></ManagerLayout>} />
+      <Route path="/manager/blogs" element={<ManagerLayout><BlogManagement /></ManagerLayout>} />
     </Routes>
   );
 };
